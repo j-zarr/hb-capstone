6251249dd5f84690122aece24d18709b69ad39c7
Julie Anzaroot <julieanzaroot@gmail.com>
2023-04-11 00:46:38 -0400

    test canvas and script tags in html connected

--- static/js/art-canvas.js
+++ static/js/art-canvas.js
@@ -1,7 +1,17 @@
-$(document).ready(() => {
-
-//$('#art-canvas')
 
 
+    if ($('#create-link').is(':disabled')) {
 
-}); // closing for document.ready
+        const artCanvas = `
+        <div>
+            <span><button>DRAW</button></span>
+            <span><button>PAINT</button></span>
+            <span><button>ERASE</button></span>
+            <span><button>SHAPEs...</button></span>
+        </div>
+        <canvas width="200" height="200" style="border:1px solid black;""></canvas>`
+
+        $('#content-area').html(artCanvas);
+
+    }
+
